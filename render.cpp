89abdd2c1a1f61e5e62b5d1178c7b2cac7a1d1ed
Ray Haleblian <ray@haleblian.com>
2022-08-24 17:24:15 +0000

    State assumptions

--- render.cpp
+++ render.cpp
@@ -10,7 +10,7 @@ OscReceiver oscReceiver;
 OscSender oscSender;
 
 // TODO support zeroconf/bonjour
-int localPort = 59588;
+int localPort = 8000;
 int remotePort = 9000;
 const char* remoteIp = "192.168.1.168";
 float fader1 = 0.5;
@@ -20,7 +20,7 @@ bool handshakeReceived;
 void on_receive(oscpkt::Message* msg, const char *what, void* arg)
 {
 	printf("%s %s\n", msg->addressPattern().c_str(), what);
-	msg->match("/3/fader2").popFloat(fader2);
+	msg->match("/pager1/1/fader1").popFloat(fader2);
 	printf("faders = %f, %f\n", fader1, fader2);
 }
 
@@ -29,7 +29,10 @@ bool setup(BelaContext *context, void *userData)
 	oscReceiver.setup(localPort, on_receive);
 	oscSender.setup(remotePort, remoteIp);
 
-	oscSender.newMessage("/3/fader1").add(0.25f).send();
+	printf("OSC initialized. Remote port is is %s:%d .\n", remoteIp, remotePort);
+	printf("Use layout 'Simple MK2'.\n");
+	
+	// oscSender.newMessage("/3/fader1").add(0.25f).send();
 	return true;
 }
 
@@ -41,7 +44,7 @@ void render(BelaContext *context, void *userData)
 	if (fader1 < 0.25)
 		v = 0.01;
 	fader1 += v;
-	oscSender.newMessage("/3/fader1").add(fader1).send();
+	// oscSender.newMessage("/3/fader1").add(fader1).send();
 }
 
 void cleanup(BelaContext *context, void *userData)
