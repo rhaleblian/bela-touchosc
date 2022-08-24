a728bf6cb431a1378b2c785eafa26ce55a8619d7
Ray Haleblian <ray@haleblian.com>
2022-08-24 20:02:55 +0000

    Read fader1 only, conservatively

--- render.cpp
+++ render.cpp
@@ -10,48 +10,53 @@ OscReceiver oscReceiver;
 OscSender oscSender;
 
 // TODO support avahi/zeroconf/bonjour _osc.tcp.local
-int receivePort = 58000;
-const char* sendAddress = "192.168.1.168";
-int sendPort = 9000;
+struct {
+	const char* address = "0.0.0.0";
+	int port = 58000;
+} receiver;
+struct {
+	const char* address = "192.168.1.168";
+	int port = 9000;
+} sender;
+int flag_message_received = 0;
 
-const char* fader1_key = "/pager1/1/fader1";
-const char* fader2_key = "/pager1/1/fader2";
-float fader1 = 0.5;
-float fader2 = 0.0;
+const char* address[4] = { "/1/fader1", "/1/fader2", "/1/fader3", "/1/fader4" };
+float value[4] = { 0.0, 0.0, 0.0, 0.0 };
 
-void on_receive(oscpkt::Message* msg, const char *sender, void* arg)
+void on_receive(oscpkt::Message* msg, const char *endpoint, void* arg)
 {
-	printf("%s %s\n", sender, msg->addressPattern().c_str());
-	msg->match(fader1_key).popFloat(fader1);
-	msg->match(fader2_key).popFloat(fader2);
-	printf("faders = %f, %f\n", fader1, fader2);
+	const char* in_address = msg->addressPattern().c_str();
+	printf("RECEIVE | ENDPOINT(%s) ADDRESS(%s)\n", endpoint, in_address);
+	if (!strcmp(in_address, address[0])) {
+		msg->match(address[0]).popFloat(value[0]);
+		flag_message_received = 1;
+	}
 }
 
 bool setup(BelaContext *context, void *userData)
 {
 	printf("Starting up.\n");
 	
-	oscReceiver.setup(receivePort, on_receive);
-	oscSender.setup(sendPort, sendAddress);
+	oscReceiver.setup(receiver.port, on_receive);
+	oscSender.setup(sender.port, sender.address);
 
-	printf("Receiving on port %d .\n", receivePort);
-	printf("Sending to port %s:%d .\n", sendAddress, sendPort);
-	printf("OSC initialized.\n");
+	printf("Receiving on port %s:%d .\n", receiver.address, receiver.port);
+	printf("Sending to port %s:%d .\n", sender.address, sender.port);
 	printf("Sender should be TouchOSC Mk2 using layout 'Simple MK2'.\n");
-	
-	// oscSender.newMessage("/3/fader1").add(0.25f).send();
+	printf("Reading %s only.\n", address[0]);
+	printf("OSC initialized.\n");
+
 	return true;
 }
 
 void render(BelaContext *context, void *userData)
 {
-	float v = 0.01;
-	if (fader1 > 0.75)
-		v = -0.01;
-	if (fader1 < 0.25)
-		v = 0.01;
-	fader1 += v;
-	// oscSender.newMessage("/3/fader1").add(fader1).send();
+	
+	if (flag_message_received) {
+		printf("%s %f\n", address[0], value[0]);
+		flag_message_received = 0;
+	}
+	// oscSender.newMessage(address[1]).add(value[1]).send();
 }
 
 void cleanup(BelaContext *context, void *userData)
